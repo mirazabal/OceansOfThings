bce4a9aec083ac64b172111faa84885b1b539d9f
mirazabal <mikel-irazabal@hotmail.com>
2017-03-18 18:54:27 +0100

    working in x64

--- IoT_Node/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikel/workspace/OceansOfThings/IoT_Node
-# Build directory: /home/mikel/workspace/OceansOfThings/IoT_Node/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
